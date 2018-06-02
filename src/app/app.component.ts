c87e4863aad0073bdab37986a5004a474ff0ab53
Ivan Romero <ivangrr@gmail.com>
2018-06-01 23:46:16 -0400

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = HomePage;
+  isAndroid: boolean = false;
+
+  //constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    constructor(platform: Platform) {
+    this.isAndroid = platform.is('android');
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      // statusBar.styleDefault();
+      // splashScreen.hide();
+    });
+  }
+}
+
